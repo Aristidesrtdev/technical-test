55d6d5802e9b3e82c67b0e12056666d3f7bb9643
Aristidesramto <gerdani@hotmail.es>
2025-11-06 21:06:04 +0000

    :sparkles: data initalizer

---
+++ src/main/java/com/twd/technicaltest/infrastructure/config/HotelDataInitializer.java
@@ -0,0 +1,34 @@
+package com.twd.technicaltest.infrastructure.config;
+
+import com.twd.technicaltest.domain.model.Address;
+import com.twd.technicaltest.domain.model.Hotel;
+import com.twd.technicaltest.domain.repository.HotelRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class HotelDataInitializer implements CommandLineRunner {
+
+    private final HotelRepository hotelRepository;
+
+    @Override
+    public void run(String... args) {
+            hotelRepository.create(new Hotel(null, "Hotel Gran Canaria", 5,
+                    new Address("Av. de Las Canteras 123", "Las Palmas", "España", "35010")));
+
+            hotelRepository.create(new Hotel(null, "Lopesan Costa Meloneras Resort & Spa", 5,
+                    new Address("Calle Mar Mediterraneo 1", "Maspalomas", "España", "35100")));
+
+            hotelRepository.create(new Hotel(null, "Riu Plaza España", 3,
+                    new Address("Gran Via  84", "Madrid", "España", "28013")));
+
+            hotelRepository.create(new Hotel(null, "Hotel del Sol", 5,
+                    new Address("Av. del Sol 99", "Málaga", "España", "29015")));
+
+            hotelRepository.create(new Hotel(null, "Hotel Atlántico", 4,
+                    new Address("Rua das Flores 22", "Oporto", "Portugal", "4000-123")));
+
+    }
+}
